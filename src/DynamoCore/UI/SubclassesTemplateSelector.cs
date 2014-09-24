b4bcc747477946ad65b8b9791dd03fec2b39fd17
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-09-24 14:49:11 +0300

    Temp commit, it doesn't work, need to merge new changes from base branch.

--- src/DynamoCore/UI/SubclassesTemplateSelector.cs
+++ src/DynamoCore/UI/SubclassesTemplateSelector.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using Dynamo.Nodes.Search;
+using Dynamo.Search.SearchElements;
 
 namespace Dynamo.Controls
 {
@@ -9,6 +10,7 @@ namespace Dynamo.Controls
     {
         public DataTemplate SubclassesTemplate { get; set; }
         public DataTemplate ClassDetailsTemplate { get; set; }
+        public DataTemplate NestedClassesTemplate { get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
@@ -16,10 +18,33 @@ namespace Dynamo.Controls
                 return ClassDetailsTemplate;
 
             if (item is BrowserRootElement)
+            {
+                if (ConsistOfNestedClasses(item as BrowserRootElement))
+                    return NestedClassesTemplate;
                 return SubclassesTemplate;
+            }
 
             const string message = "Unknown object bound to collection";
             throw new InvalidOperationException(message);
+        }
+
+        private bool ConsistOfNestedClasses(BrowserRootElement rootElement)
+        {
+            // Go deeper in item for 2 levels. 1st level - class button,
+            // 2nd level - class member.
+            // E.g. 1st lvl - Color, 2nd lvl - Red.
+            // But for nested classes 2nd lvl will be BrowserInternalElement,
+            // That's how we know whether it is nested structure or not.
+            foreach (var classItem in rootElement.Items)
+            {
+                foreach (var classMember in classItem.Items)
+                {
+                    if (classMember is SearchElementBase) continue;
+                    else return true;
+                }
+            }
+
+            return false;
         }
     }
 }
