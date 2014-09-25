403d36d39e5b083e1c804522f051b2db78e27413
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-09-25 16:25:22 +0300

    Finally it works.

---
+++ src/DynamoCore/UI/NestedClassesTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Dynamo.Nodes.Search;
+
+namespace Dynamo.Controls
+{
+    public class NestedClassesTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SubclassesTemplate { get; set; }
+        public DataTemplate ClassDetailsTemplate { get; set; }
+        public DataTemplate NestedClassesTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is ClassInformation)
+                return ClassDetailsTemplate;
+
+            if (item is BrowserItem)
+            {
+                if (Dynamo.Nodes.Utilities.ConsistOfNestedClasses(item as BrowserItem)) 
+                    return NestedClassesTemplate;
+
+                return SubclassesTemplate;
+            }
+
+            const string message = "Unknown object bound to collection";
+            throw new InvalidOperationException(message);
+        }
+    }
+}
