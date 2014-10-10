af1cc23194bcbfc0ec2f527d4c24f3200bc9745d
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-10-10 12:53:04 +0300

    Moving to LibrarySearchView.

--- src/DynamoCore/UI/Views/LibrarySearchView.xaml.cs
+++ src/DynamoCore/UI/Views/LibrarySearchView.xaml.cs
@@ -42,5 +42,62 @@ namespace Dynamo.UI.Views
             scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
             e.Handled = true;
         }
+
+        // Here we can move left, right, up, down.
+        private void OnClassButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            var classButton = sender as ListViewItem;
+            var listViewButtons = Dynamo.Utilities.WPF.FindUpVisualTree<ListView>(classButton);
+            var selectedIndex = listViewButtons.SelectedIndex;
+            int itemsPerRow = (int)Math.Floor(listViewButtons.ActualWidth / classButton.ActualWidth);
+
+            int newIndex = GetIndexNextSelectedItem(e.Key, selectedIndex, itemsPerRow);
+
+            if ((newIndex >= 0) && (newIndex < listViewButtons.Items.Count))
+                listViewButtons.SelectedIndex = newIndex;
+
+            // Set focus on new selected item.
+            var item = listViewButtons.ItemContainerGenerator.ContainerFromIndex(listViewButtons.SelectedIndex) as ListViewItem;
+            item.Focus();
+
+            e.Handled = true;
+            return;
+        }
+
+        private int GetIndexNextSelectedItem(Key key, int selectedIndex, int itemsPerRow)
+        {
+            int newIndex = -1;
+            int numberOfSelectedRow = selectedIndex / itemsPerRow + 1;
+
+            switch (key)
+            {
+                case Key.Right:
+                    {
+                        newIndex = selectedIndex + 1;
+                        int availableIndex = numberOfSelectedRow * itemsPerRow - 1;
+                        if (newIndex > availableIndex) newIndex = selectedIndex;
+                        break;
+                    }
+                case Key.Left:
+                    {
+                        newIndex = selectedIndex - 1;
+                        int availableIndex = (numberOfSelectedRow - 1) * itemsPerRow;
+                        if (newIndex < availableIndex) newIndex = selectedIndex;
+                        break;
+                    }
+                case Key.Down:
+                    {
+                        newIndex = selectedIndex + itemsPerRow + 1;
+                        // +1 because one of items is always ClassInformation.
+                        break;
+                    }
+                case Key.Up:
+                    {
+                        newIndex = selectedIndex - itemsPerRow;
+                        break;
+                    }
+            }
+            return newIndex;
+        }
     }
 }
